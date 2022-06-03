f9a2165d7df72e67d4eec139c8c7fbbde2126e65
YoannJoly <45359415+YoannJoly@users.noreply.github.com>
2022-06-03 16:36:10 +0200

    tp5_3

--- Diginamic/src/tp5_encapsulation/banque/TestBanque.java
+++ Diginamic/src/tp5_encapsulation/banque/TestBanque.java
@@ -6,6 +6,7 @@ public class TestBanque {
 
     public static void main(String[] args) {
         Compte compte = new Compte("15456444154", 654);
+        System.out.println(compte);
         System.out.println(compte.getAccoundSold() + " " + compte.getAccountNumber());
     }
 }
