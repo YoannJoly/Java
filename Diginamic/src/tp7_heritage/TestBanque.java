362e894a84702f9ee78f98afca434719faf31c42
YoannJoly <45359415+YoannJoly@users.noreply.github.com>
2022-06-07 10:02:57 +0200

    tp7

--- Diginamic/src/tp7_heritage/TestBanque.java
+++ Diginamic/src/tp7_heritage/TestBanque.java
@@ -6,7 +6,7 @@ import tp7_heritage.entites.CompteTaux;
 public class TestBanque {
 
     public static void main(String[] args) {
-        Compte[] comptes = { new tp7_heritage.entites.Compte("4594894", 200), new CompteTaux("45646", 1200, 2) };
+        Compte[] comptes = { new Compte("4594894", 200), new CompteTaux("45646", 1200, 2) };
         System.out.println(comptes[0] + " " + comptes[1]);
     }
 
