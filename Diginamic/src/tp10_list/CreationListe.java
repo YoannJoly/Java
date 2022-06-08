77c621b7c8a9aa7b7e9cd9dc3f4c6050a78e098f
YoannJoly <45359415+YoannJoly@users.noreply.github.com>
2022-06-08 12:48:20 +0200

    tp10_ex1-2-3

---
+++ Diginamic/src/tp10_list/CreationListe.java
@@ -0,0 +1,16 @@
+package tp10_list;
+
+import java.util.ArrayList;
+
+public class CreationListe {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 1; i <= 100; i++) {
+            list.add(i);
+        }
+        // System.out.println(list);
+        System.out.println(list.size());
+    }
+
+}
---
+++ Diginamic/src/tp10_list/TestListeInt.java
@@ -0,0 +1,28 @@
+package tp10_list;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class TestListeInt {
+    static List<Integer> list = new ArrayList<Integer>(Arrays.asList(-1, 5, 7, 3, -2, 4, -8, 5));
+
+    public static void main(String[] args) {
+        System.out.println("Element de la liste: " + list);
+        System.out.println("Taille de la liste: " + list.size());
+        System.out.println("Plus grand élément de la liste: " + Collections.max(list));
+
+        list.remove(Collections.min(list));
+        System.out.println("Plus petit élément supprimmé: " + list);
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) < 0) {
+                list.set(i, Math.abs(list.get(i)));
+            }
+        }
+
+        System.out.println("Element négatif devenu positif: " + list);
+
+    }
+}
